dc581c41085201b9ce6cce1faca5e195b5cdc106
Timo van den Berg <mrtimogames@gmail.com>
2021-11-24 21:48:50 +0100

    Eerste animatie, maar interactie nog niet helemaal werkend

---
+++ js/eersteAnimatie.js
@@ -0,0 +1,11 @@
+const svg = document.getElementById("triangles");
+
+svg.onclick = (e) => {
+    console.log("hoi");
+    const colors = ['red', 'blue', 'green', 'orange', 'pink', 'purple']
+    const rando = () => colors[Math.floor(Math.random() * colors.length)];
+    document.documentElement.style.cssText = `
+          --dark-color: ${rando()};
+          --light-color: ${rando()}; 
+          `
+}
