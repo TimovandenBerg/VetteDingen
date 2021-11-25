c247daf7257bb55f7ba2d7b3abdc35a579afeec3
Timo van den Berg <mrtimogames@gmail.com>
2021-11-25 13:21:56 +0100

    Meer kleuren toegevoegd
    Door het toevoegen van meerdere kleuren is de kans dat de driehoeken dezelfde kleur krijgen minder geworden.

--- js/eersteAnimatie.js
+++ js/eersteAnimatie.js
@@ -5,7 +5,12 @@ const darkGroup = document.getElementById("darkGroup");
 svg.onclick = (e) => {
     // var oudeKleur = window.getComputedStyle(ani1White, null).getPropertyValue("fill");
 
-    const colors = ['red', 'blue', 'green', 'orange', 'pink', 'purple']
+    const colors = ['red', 'blue', 'green', 'orange', 'pink', 'purple', 'black', 
+                    'grey', 'lightblue', 'yellowgreen', 'violet', 'turquoise', 'tomato',
+                    'teal', 'thistle', 'tan', 'steelblue', 'springgreen', 'slateblue', 
+                    'seagreen', 'salmon', 'sandybrown', 'plum', 'peru', 'peachpuff', 
+                    'oldlace', 'maroon', 'limegreen', 'lightskyblue', 'lightseagreen', 'lightsalmon', 
+                    'ivory', 'khaki'];
     const groepen = [darkGroup, ani1White];
     const rando = () => colors[Math.floor(Math.random() * colors.length)];
     for (i = 0; i < groepen.length; i++) {
