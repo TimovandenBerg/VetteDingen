0ee1719a961818fdb933c207c2da922cf76948e3
Timo van den Berg <mrtimogames@gmail.com>
2021-11-25 13:09:35 +0100

    De  kleuren van de driehoeken kunnen worden veranderd en dat gebeurd elke keer. Lukt alleen nog niet dat de kleuren niet dezelfde mogen zijn als de vorige keer.

--- js/eersteAnimatie.js
+++ js/eersteAnimatie.js
@@ -1,11 +1,19 @@
 const svg = document.getElementById("triangles");
+const ani1White = document.getElementById("ani1--white1");
+const darkGroup = document.getElementById("darkGroup");
 
 svg.onclick = (e) => {
+    // var oudeKleur = window.getComputedStyle(ani1White, null).getPropertyValue("fill");
+
     const colors = ['red', 'blue', 'green', 'orange', 'pink', 'purple']
+    const groepen = [darkGroup, ani1White];
     const rando = () => colors[Math.floor(Math.random() * colors.length)];
-    document.documentElement.style.cssText = `
-          --dark-color: ${rando()};
-          --light-color: ${rando()}; 
-          `
-    console.log(svg);
+    for (i = 0; i < groepen.length; i++) {
+        nieuweKleur = rando();
+        // while (oudeKleur === nieuweKleur) {
+        //     nieuweKleur = rando();
+        //     console.log("Zijn hetzelfde");
+        // }
+        groepen[i].style.fill = nieuweKleur;
+    }
 }
